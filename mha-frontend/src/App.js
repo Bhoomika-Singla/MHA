9eff0ecc9bffb514371e0e409c4747111d0d786f
Manali Kale <manaliakale@gmail.com>
2023-04-01 19:31:34 -0600

    Updating app header and timeline slider

--- mha-frontend/src/App.js
+++ mha-frontend/src/App.js
@@ -1,23 +1,67 @@
-import { useState } from "react";
-import RangeSlider from "react-range-slider-input";
-import "react-range-slider-input/dist/style.css";
-import "./App.css";
+import React from "react";
+import { format } from "date-fns";
+import TimeRange from "react-timeline-range-slider";
+import './App.css';
 
-export default function App() {
-  const [value, setValue] = useState([2000, 2022]);
+import {
+  selectedInterval,
+  timelineInterval
+} from "./datesSource";
 
-  return (
-    <>
-      
-      <RangeSlider
-        min={1950}
-        max={2022}
-        id="range-slider-gradient"
-        className="margin-lg"
-        step={1}
-        defaultValue = {value}
-      />
 
-    </>
-  );
+class App extends React.Component {
+  state = {
+    error: false,
+    selectedInterval
+  };
+
+  errorHandler = ({ error }) => this.setState({ error });
+
+  onChangeCallback = (selectedInterval) => this.setState({ selectedInterval });
+
+  render() {
+    const { selectedInterval, error } = this.state;
+    return (
+      <div>
+        <div class="app-header">
+          <div class="header1">
+            Music
+          </div>
+          <div class="header2">
+            History
+          </div>
+        </div>
+        <div>
+          <div class="interval">
+            <span>Select interval by : </span>
+            <button class="interval-button" type="button">Year</button>
+            <button class="interval-button" type="button">Month</button>
+            <button class="interval-button" type="button">Day</button>
+          </div>
+
+          <div class="interval">
+            <span>Selected Interval: </span>
+            {selectedInterval.map((d, i) => (
+              <span class="interval-date" key={i}>{format(d, "yyyy")}</span>
+            ))}
+          </div>
+
+          <div>
+            <TimeRange
+              error={error}
+              ticksNumber={25}
+              selectedInterval={selectedInterval}
+              timelineInterval={timelineInterval}
+              onUpdateCallback={this.errorHandler}
+              onChangeCallback={this.onChangeCallback}
+              formatTick={ms => format(new Date(ms), 'yyyy')}
+              step={2592000000}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+export default App;
